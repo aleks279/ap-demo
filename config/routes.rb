26919e0e2a1d84df62c0bf649b188f2539b11c68
Saul Zamora <szamora@pernix-solutions.com>
2018-04-11 17:54:49 -0600

    Add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :sections
-  resources :pages
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get 'admin/login' => 'users/sessions#new'
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  namespace :admin do
+    resources :pages
+  end
+
+  resources :pages, only: [:show]
 
   root 'home#index'
 end
